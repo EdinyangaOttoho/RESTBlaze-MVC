d2755902dff501ed98e8300e9f2787906b7e8558
Edinyanga O. Ottoho <elzucky@gmail.com>
2021-08-10 23:07:00 +0100

    Update launcher.php

--- launcher.php
+++ launcher.php
@@ -1,4 +1,7 @@
 <?php
+    //clean .htaccess
+    file_put_contents("./.htaccess", "");
+
     //Load initial configs for .htaccess and handlers
     include("./server/manager.php");
     include("./server/dotenv.php");
